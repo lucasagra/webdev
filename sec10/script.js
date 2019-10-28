205fb47838b2e2888a0e4e99fd4b4e7490b0c53c
Lucas <lucasomena@hotmail.com>
2019-10-28 00:28:17 -0300

    section 10 done

---
+++ sec10/script.js
@@ -0,0 +1,6 @@
+let fName = prompt("What's your first name?");
+let lName = prompt("What's your last name?");
+let age = prompt("What's your age?");
+console.log("Your full name is " + fName + " " + lName);
+console.log("You are " + age + " years old");
+console.log("You've been alive for " + age*365.25 + " days");
