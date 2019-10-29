c3cb27be73f55ff37c48937d5e78ad88037c7c97
Lucas <lucasomena@hotmail.com>
2019-10-29 00:52:36 -0300

    section 15 done

---
+++ sec15/selector.js
@@ -0,0 +1,6 @@
+let p11 = document.querySelector("#first")
+let p12 = document.querySelector(".special")
+let p13 = document.querySelector("p")
+let p14 = document.getElementById("first")
+let p15 = document.getElementsByTagName("p")[0]
+let p16 = document.getElementsByClassName("special")[0]
